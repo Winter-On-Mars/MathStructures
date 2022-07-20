5995f4b71fe6711f82913385d9c0acac5f6bc185
PIE4LIF3 <izik.pfirrmann@gmail.com>
2022-07-20 00:50:25 -0500

    Did timing testing for the matrix multiplication operation

--- MathStructures/src/main.cpp
+++ MathStructures/src/main.cpp
@@ -1,25 +1,36 @@
 #include <iostream>
+#include <chrono>
 #include "../Structures/MathStructs.h"
 
-void func() {
 
-}
 
 int main() {
 	std::cout << "Hello World!\n";
 	
-	double ihatethis[] = { 1, 2, 3, 4, 5, 6 };
-	double ihatethis2electricboogoolo[] = { 10, 11, 20, 21, 30, 31 };
+	double ihatethis[] = { 1, 1, 0, 0, 0, 1 };
+	double ihatethis2electricboogoolo[] = { 1, 0, 0, 1, 0, 1 };
 
-	math::Matrix m1 = math::Matrix(2, 3, ihatethis);
-	math::Matrix m2 = m1.inverse();
+	math::Matrix m1 = math::Matrix(3, 2, ihatethis);
+	math::Matrix m2 = math::Matrix(2, 3, ihatethis2electricboogoolo);
+	
+	auto start1 = std::chrono::high_resolution_clock::now();
+	math::Matrix m3 = m1 * m2;
+	auto stop1 = std::chrono::high_resolution_clock::now();
+	auto dur1 = std::chrono::duration_cast<std::chrono::microseconds>(stop1 - start1);
+	
+	auto start2 = std::chrono::high_resolution_clock::now();
+	math::Matrix m4 = m1.matmult(m1, m2);
+	auto stop2 = std::chrono::high_resolution_clock::now();
+	auto dur2 = std::chrono::duration_cast<std::chrono::microseconds>(stop2 - start2);
 
+	auto start3 = std::chrono::high_resolution_clock::now();
+	math::Matrix m5 = m1.matmult(m2);
+	auto stop3 = std::chrono::high_resolution_clock::now();
+	auto dur3 = std::chrono::duration_cast<std::chrono::microseconds>(stop3 - start3);
 
-	std::cout << m1;
-
-	std::cout << "--------\n";
-
-	std::cout << m2;
+	std::cout << dur1.count() << '\n';
+	std::cout << dur2.count() << '\n';
+	std::cout << dur3.count() << '\n';
 
 	return 0;
 }
