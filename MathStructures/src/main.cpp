b55dccbcca9431c447e3a00bde934ad6f6851d79
Izik Pfirrmann <izik.pfirrmann@gmail.com>
2022-08-09 23:40:37 -0500

    Implimented matrix inverse function. Added demonstration in main function.

--- MathStructures/src/main.cpp
+++ MathStructures/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <chrono>
+// #include <chrono>
 #include <vector>
+#include <optional>
 #include "../Structures/MathStructs.h"
 
 // how to get time
@@ -13,24 +14,48 @@
 // math::Matrix m(2, 2, { 1.0, 0.0, 0.0, 1.0 });
 
 int main() {
-	{
-		double m1arr[] = { 1, 2, 3, 4 };
-		double m2arr[] = { 5, 4, 3, 2 };
-		math::Matrix m1(2, 2, m1arr);
-		math::Matrix m2(2, 2, m2arr);
+	int s1 = 2;
+	int s2 = 3;
+	int s3 = 4;
+	double arr1[] = {
+		2, 3,
+		5, 2
+	};
+	double arr2[] = {
+		1, 3, 2,
+		3, 1, 4,
+		1, 3, 3
+	};
+	double arr3[] = { 
+		2, 3, 1, 5,
+		2, 4, 2, 4,
+		4, 1, 0, 2,
+		2, 6, 6, 2
+	};
+	math::Matrix m1(s1, s1, arr1);
+	math::Matrix m2(s2, s2, arr2);
+	math::Matrix m3(s3, s3, arr3);
 
-		std::cout << m1 + m2 << m1 - m2;
-	}
+	std::cout << "original\n" << m1 << m2 << m3;
 
-	{
-		double v1arr[] = { 5, 4, 3, 2, 1 };
-		math::Vector v1(5, v1arr);
-		math::Vector v2(5,
-			[](double x) -> double { return x; }
-		);
+	math::Matrix t1(s1, s1);
+	math::Matrix t2(s2, s2);
+	math::Matrix t3(s3, s3);
+	
+	t1 = m1.inverse();
+	t2 = m2.inverse();
+	t3 = m3.inverse();
 
-		std::cout << v1 + v2 << v1 - v2;
-	}
+	std::cout << "testing 1\n";
+	std::cout <<  (m1 * t1);
+	std::cout <<  (t1 * m1);
 
+	std::cout << "testing 2\n";
+	std::cout <<  (m2 * t2);
+	std::cout <<  (t2 * m2);
+	
+	std::cout << "testing 3\n";
+	std::cout <<  (m3 * t3);
+	std::cout <<  (t3 * m3);
 	return 0;
 }
